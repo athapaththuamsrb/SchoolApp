030d177eb108d56a82dbceae292e32ad8aac10d7
Rukshan Athapaththu <86597651+athapaththuamsrb@users.noreply.github.com>
2021-07-18 15:14:40 +0530

    4

--- src/StudentFormController.java
+++ src/StudentFormController.java
@@ -1,7 +1,10 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -18,12 +21,19 @@ public class StudentFormController {
     public TextField txtName;
     public TextField txtContact;
     public TextField txtAddress;
-    public TableView tblStudent;
+    public TableView tblStudents;
     public TableColumn colId;
     public TableColumn colContact;
     public TableColumn colName;
     public TableColumn colAddress;
 
+    public  void initialize(){
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colContact.setCellValueFactory(new PropertyValueFactory<>("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        loadAllStudents();
+    }
     private void loadAllStudents() {
         Configuration configuration =
                 new Configuration()
@@ -34,6 +44,15 @@ public class StudentFormController {
         Query query = session.createQuery("FROM Student");/*HQL(hibernate query language)*/
                 List list = query.list();
         System.out.println(list);
+        ObservableList<StudentTM> observableList = FXCollections.observableArrayList();
+
+        for (Object student : list) {
+            observableList.add(new StudentTM(
+                    ((Student)student).getId(),((Student)student).getName(),((Student)student).getContact(),((Student)student).getAddress()
+            ));
+        }
+
+        tblStudents.setItems(observableList);
     }
     public void btnSaveStudetOnAction(ActionEvent actionEvent) {
         Student s1= new Student(
@@ -58,8 +77,45 @@ public class StudentFormController {
     }
 
     public void btnUpDateStudetOnAction(ActionEvent actionEvent) {
+        Student s1 = new Student(
+                txtId.getText(),
+                txtName.getText(),
+                txtContact.getText(),
+                txtAddress.getText()
+        );
+
+        Configuration configuration =
+                new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Student.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(s1);
+        transaction.commit();
+        loadAllStudents();
+        session.close();
+        sessionFactory.close();
     }
 
     public void btnDeleteStudetOnAction(ActionEvent actionEvent) {
+        Student s1 = new Student(
+                txtId.getText(),
+                txtName.getText(),
+                txtContact.getText(),
+                txtAddress.getText()
+        );
+        Configuration configuration =
+                new Configuration()
+                        .configure("hibernate.cfg.xml")
+                        .addAnnotatedClass(Student.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(s1);
+        transaction.commit();
+        loadAllStudents();
+        session.close();
+        sessionFactory.close();
     }
 }
